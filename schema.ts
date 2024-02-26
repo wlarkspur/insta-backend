6bf93454a7d6da25165b2256533c9f4af1765941
wlarkspur <newacon@naver.com>
2024-02-26 23:07:08 +0200

    server delete

---
+++ schema.ts
@@ -0,0 +1,15 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.ts`);
+// /**/*.  모든 폴더, 모든 파일을 검색
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.ts`
+);
+const typeDefs = mergeTypeDefs(loadedTypes);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
--- server.ts
+++ server.ts
@@ -1,66 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-
+import "dotenv/config";
 import { ApolloServer, gql } from "apollo-server";
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-interface IMutation {
-  id: number;
-  title: string;
-  year: number;
-  genre: string;
-}
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_: any, { id }: { id: number }) =>
-      client.movie.findUnique({ where: { id } }),
-  },
-
-  Mutation: {
-    createMovie: (_: any, { title, year, genre }: IMutation) => {
-      const movie = client.movie.create({
-        data: {
-          title,
-          year,
-          genre,
-        },
-      });
-      return movie;
-    },
-    deleteMovie: (_: any, { id }: IMutation) =>
-      client.movie.delete({ where: { id } }),
-    updateMovie: (_: any, { id, year }: IMutation) =>
-      client.movie.update({ where: { id }, data: { year } }),
-  },
-};
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() =>
+    console.log(`🚀 Server is running on http://localhost:${PORT}/ ✅`)
+  );
