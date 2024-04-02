bb983538c49652b6aee87ce4b7fa6edfac63b2ed
wlarkspur <newacon@naver.com>
2024-04-02 22:31:47 +0200

    roomUpdated filtering

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,11 +1,32 @@
+import { withFilter } from "apollo-server-express";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pusub";
 import { Resolvers } from "../../types";
+import client from "../../client";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!room) {
+          throw new Error("You shall not see this :)");
+        }
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+            return true;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
