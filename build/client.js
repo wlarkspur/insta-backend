ddbc1baacc4f1f8c2a4edcfb0aa650c2202bdb85
wlarkspur <newacon@naver.com>
2024-08-29 00:39:18 +0200

    fuck

--- build/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _client = require("@prisma/client");
-var client = new _client.PrismaClient();
-var _default = exports["default"] = client;
